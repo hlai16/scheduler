79d38002d61562efa44237b75a4f2fc2856388f3
hlai16 <gloriajoanlai@gmail.com>
2021-10-26 16:27:58 -0400

    button addClass in a more efficient way only partially solved

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,21 +1,22 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   let buttonClass = "button";
+   // let buttonClass = "button";
  
-   if (props.confirm) {
-     buttonClass += " button--confirm";
-   }
+   // if (props.confirm) {
+   //   buttonClass += " button--confirm";
+   // }
  
-   if (props.danger) {
-     buttonClass += " button--danger";
-   }
- 
+   // if (props.danger) {
+   //   buttonClass += " button--danger";
+   // }
+   
    return (
      <button
-       className={buttonClass}
+       className={classNames('button', { 'button--danger': true })}
        onClick={props.onClick}
        disabled={props.disabled}
      >
