591e1b806bd4e4f03da2f78d5fe62ae024b8f950
hlai16 <gloriajoanlai@gmail.com>
2021-10-26 15:57:57 -0400

    test run to verify all buttons worked

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,13 +8,18 @@ export default function Button(props) {
    if (props.confirm) {
      buttonClass += " button--confirm";
    }
+ 
    if (props.danger) {
      buttonClass += " button--danger";
    }
-   if (props.disabled) {
-     buttonClass += " button--disabled";
-   }
-
  
-   return <button className={buttonClass}>{props.children}</button>;
+   return (
+     <button
+       className={buttonClass}
+       onClick={props.onClick}
+       disabled={props.disabled}
+     >
+       {props.children}
+     </button>
+   );
  }
