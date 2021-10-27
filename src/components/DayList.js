282e00b835a037537ef986ca03862f9866bd3ad4
hlai16 <gloriajoanlai@gmail.com>
2021-10-27 00:07:07 -0400

    debugging by myself

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,8 +4,8 @@ import DayListItem from "components/DayListItem.js";
 // import classNames from "classnames";
 
 export default function DayList(props) {
-    const { days, setDay } = props;
-    const mapDaysArr = days.map(day => <DayListItem key={props.day.id} name={props.day.name} spots={props.day.spots} selected={props.day.name === props.day} setDay={props.setDay} {...day} />);
+    const { days } = props;
+    const mapDaysArr = days.map(day => <DayListItem key={day.id} name={day.name} spots={day.spots} selected={day.name === props.day} setDay={props.setDay} {...day} />);
     return (
         <ul>{mapDaysArr}</ul>
     );
