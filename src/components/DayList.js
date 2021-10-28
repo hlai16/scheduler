45bd21fec30c6e1bdb3e044c212202ffa6b37c72
hlai16 <gloriajoanlai@gmail.com>
2021-10-27 22:37:08 -0400

    writing stories

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,8 +4,8 @@ import DayListItem from "components/DayListItem.js";
 // import classNames from "classnames";
 
 export default function DayList(props) {
-    const { days } = props;
-    const mapDaysArr = days.map(day => <DayListItem key={day.id} name={day.name} spots={day.spots} selected={day.name === props.day} setDay={props.setDay} {...day} />);
+    // const { days } = props;
+    const mapDaysArr = props.days.map(day => <DayListItem key={day.id} name={day.name} spots={day.spots} selected={day.name === props.value} setDay={props.onChange} {...day} />);
     return (
         <ul>{mapDaysArr}</ul>
     );
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,7 +4,7 @@ import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
     // const { interviewers } = props;
-    const mapInterviewersArr = props.interviewers.map(interviewer => <InterviewerListItem key={interviewer.id} name={interviewer.name} avatar={interviewer.avatar} selected={interviewer.id === props.interviewer} setInterviewer={props.setInterviewer} {...interviewer} />);
+    const mapInterviewersArr = props.interviewers.map(interviewer => <InterviewerListItem key={interviewer.id} name={interviewer.name} avatar={interviewer.avatar} selected={interviewer.id === props.value} setInterviewer={() => props.onChange(interviewer.id)} {...interviewer} />);
     return (
         <section className="interviewers">
             <h4 className="interviewers__header text--light">Interviewer</h4>
