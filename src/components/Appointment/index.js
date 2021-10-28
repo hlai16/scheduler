32af10a319d1f2cab44af6a94db66d898cfaa727
hlai16 <gloriajoanlai@gmail.com>
2021-10-27 23:15:30 -0400

    attempt to build Header

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,12 @@
 import React from "react";
-import 'Appointment/styles.scss';
-
+import './styles.scss';
+import Header from "./Header";
 
 export default function Appointment(props) {
-    
+    const formatAppointment = function (time) {
+        return time ? `Appointment at ${time}.` : 'No Appointments'
+    }
     return (
-        <article className="appointment"></article>
+        <article className="appointment">{formatAppointment(props.time)}</article>
     );
 }
