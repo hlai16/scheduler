f50643001ac5749268d6a70a76963b369725c3a9
hlai16 <gloriajoanlai@gmail.com>
2021-10-27 23:25:20 -0400

    attempt to write Empty story

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -2,7 +2,6 @@ import React from "react";
 
 
 export default function Header(props) {
-    const { time: String } = props;
     return (
         <header className="appointment__time">
             <h4 className="text--semi-bold">{props.time}</h4>
