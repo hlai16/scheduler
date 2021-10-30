7f0c538f0ddf60db346cfa0648ea52c08dd8f62d
hlai16 <gloriajoanlai@gmail.com>
2021-10-30 06:49:24 -0400

    trying to understand transition mode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,4 +1,15 @@
+import { useState } from "react";
 
-export default function useVisualMode(FIRST) {
-    return { FIRST };
+const useVisualMode = (initial) => {
+    const [mode, setMode] = useState(initial);
+    const transition = (newMode, replace) => {
+        if (replace) {
+            let replaceMode = [...mode];
+            replaceMode[replaceMode.length - 1] = mode;
+            setMode((prev) => newMode);
+        }
+    }
+    return { mode, transition };
 }
+
+export default useVisualMode;
