2e131ded251dcc24aa49507ee67c0e29f1fdc370
hlai16 <gloriajoanlai@gmail.com>
2021-11-04 10:41:01 -0400

    attempting to use data-id

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,11 +1,10 @@
 describe("Navigation", () => {
-    it("should visit root", () => {
-      cy.visit("/");
-    });
-    it('should navigate to Tuesday', () => {
-      cy.contains('Tuesday')
-      .click()
-      cy.get(".day-list__item--selected")
-      .should('contain', 'Tuesday')
-    });
+  it("should visit root", () => {
+    cy.visit("/");
   });
+  it('should navigate to Tuesday', () => {
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .contains('.day-list__item--selected');
+  });
+});
